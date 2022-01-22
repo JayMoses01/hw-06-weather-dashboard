be192bc5836c59ca1ee94603332d5c934f04ff97
JayMoses01 <95326705+JayMoses01@users.noreply.github.com>
2022-01-22 00:43:30 -0800

    Adding script.js file

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Moments.js to replace "current-date" with today.
+
+
+
+
+
+// Search button event listener to commit to local storage, add a searched city button, and populate "today-card" and forecast cards.
+
+
